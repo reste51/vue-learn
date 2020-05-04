f588bd28d5612e5d624bc35948928a0adf8be7a4
reste51 <502042437@qq.com>
2020-05-04 22:12:20 +0800

    测试

---
+++ src/components/eventBus/com1_1.vue
@@ -0,0 +1,19 @@
+<template>
+  <div @click="fbEvent">
+    这是 comp1 1111111组件
+  </div>
+</template>
+
+<script>
+import { eventBus } from '../../eventBus'
+export default {
+  methods: {
+    // 发布一个事件
+    fbEvent() {
+      eventBus.$emit('comClick')
+    }
+  }
+}
+</script>
+
+<style scoped></style>
---
+++ src/components/eventBus/com2_2.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    这是 comp2 2222组件
+  </div>
+</template>
+
+<script>
+import { eventBus } from '../../eventBus'
+export default {
+  mounted() {
+    eventBus.$on('comClick', () => {
+      alert(' component 1 is clicked  from com 2')
+    })
+  }
+}
+</script>
+
+<style scoped></style>
